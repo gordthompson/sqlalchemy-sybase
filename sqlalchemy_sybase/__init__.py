a3bec973913ad687b8f26fd6fe5f4c9296af35ae
gordthompson <gord@gordthompson.com>
2020-06-08 15:09:51 -0600

    Prepare for next iteration

--- sqlalchemy_sybase/__init__.py
+++ sqlalchemy_sybase/__init__.py
@@ -34,7 +34,7 @@ from .base import VARBINARY
 from .base import VARCHAR
 from .pyodbc import get_odbc_info
 
-__version__ = "1.0.3"
+__version__ = "1.0.4b1"
 
 # default (and only) dialect
 base.dialect = dialect = pyodbc.dialect
