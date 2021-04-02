aefcd477870846e313efef96fd47ae6134fc5a90
gordthompson <gord@gordthompson.com>
2021-04-02 17:09:51 -0600

    Bump version

--- sqlalchemy_sybase/__init__.py
+++ sqlalchemy_sybase/__init__.py
@@ -34,7 +34,7 @@ from .base import VARBINARY
 from .base import VARCHAR
 from .pyodbc import get_odbc_info
 
-__version__ = "1.0.6"
+__version__ = "1.0.7b1.dev0"
 
 # default (and only) dialect
 base.dialect = dialect = pyodbc.dialect
